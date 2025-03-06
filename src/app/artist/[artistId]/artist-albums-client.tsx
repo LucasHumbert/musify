6e604312d488be7df4b49a22c036331e36b97789
Lucas Humbert <lucas.humbert.28@gmail.com>
2025-03-06 17:10:01 +0100

    wip loading

--- src/app/artist/[artistId]/artist-albums-client.tsx
+++ src/app/artist/[artistId]/artist-albums-client.tsx
@@ -3,7 +3,6 @@
 import { Album } from "@/utils/spotify/spotifyTypes";
 import AlbumPreview from "@/components/album/album-preview";
 import { useState, useEffect, useMemo } from "react";
-import LoadingSpinner from "@/components/loading-spinner";
 
 export default function ArtistAlbumsClient({ content }: { content: { albums: Album[], singles: Album[], appears_on: Album[] } }) {
     const defaultCategory: 'albums' | 'singles' | 'appears_on' =
@@ -23,22 +22,28 @@ export default function ArtistAlbumsClient({ content }: { content: { albums: Alb
     return (
         <section className='mt-12'>
             <div className="flex gap-4 mb-4">
-                {!isLoading && (['albums', 'singles', 'appears_on'] as const).map((category) => (
-                    <button
-                        key={category}
-                        onClick={() => setSelectedCategory(category)}
-                        className={`text-2xl font-medium cursor-pointer ${
-                            selectedCategory === category ? "text-green-500 underline" : "text-gray-700"
-                        }`}
-                        disabled={isLoading}
-                    >
-                        {category.charAt(0).toUpperCase() + category.slice(1)}
-                    </button>
-                ))}
+                {!isLoading && (['albums', 'singles', 'appears_on'] as const).map((category) => {
+                    if (content[category].length) {
+                        return <button
+                            key={category}
+                            onClick={() => setSelectedCategory(category)}
+                            className={`text-2xl font-medium cursor-pointer ${
+                                selectedCategory === category ? "text-green-500 underline" : "text-gray-700"
+                            }`}
+                            disabled={isLoading}
+                        >
+                            {category.charAt(0).toUpperCase() + category.slice(1)}
+                        </button>
+                    }
+                })}
             </div>
 
             {isLoading ? (
-                <div className='flex justify-center items-center'><LoadingSpinner /></div>
+                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
+                    {[...Array(6)].map((_, i) => (
+                        <div key={i} className="w-full aspect-square bg-gray-400 animate-pulse"></div>
+                    ))}
+                </div>
             ) : (
                 <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
                     {contentToDisplay.length ? contentToDisplay.map((album) => (
