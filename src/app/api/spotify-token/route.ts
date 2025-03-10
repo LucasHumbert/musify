91e832becec50491efa204cbb72002fbb0545d12
Lucas Humbert <lucas.humbert.28@gmail.com>
2025-03-10 16:05:48 +0100

    wip deployment

--- src/app/api/spotify-token/route.ts
+++ src/app/api/spotify-token/route.ts
@@ -4,12 +4,16 @@ let cachedToken: string | null = null;
 let tokenExpiration: number = 0;
 
 export async function GET() {
+    console.log('API appelée')
+
     if (cachedToken && tokenExpiration > Date.now()) {
         return NextResponse.json({ access_token: cachedToken });
     }
 
     const client_id = process.env.SPOTIFY_CLIENT_ID!;
     const client_secret = process.env.SPOTIFY_CLIENT_SECRET!;
+
+    console.log('avant appel spotify')
 
     const response = await fetch("https://accounts.spotify.com/api/token", {
         method: "POST",
@@ -24,7 +28,13 @@ export async function GET() {
         }),
     });
 
+    console.log('apres appel spotify')
+
+
     if (!response.ok) {
+
+        console.log('response pas ok')
+
         return NextResponse.json(
             { error: "Erreur lors de l'obtention du token" },
             { status: 500 }
@@ -32,6 +42,8 @@ export async function GET() {
     }
 
     const data = await response.json();
+
+    console.log('data: ', data)
 
     cachedToken = data.access_token;
     tokenExpiration = Date.now() + data.expires_in * 1000;
