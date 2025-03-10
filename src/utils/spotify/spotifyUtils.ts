8be62aa6b97488c2ce47f92aacd312b1921ad96b
Lucas Humbert <lucas.humbert.28@gmail.com>
2025-03-10 15:47:55 +0100

    wip deployment

--- src/utils/spotify/spotifyUtils.ts
+++ src/utils/spotify/spotifyUtils.ts
@@ -1,16 +1,16 @@
 export async function fetchSpotifyToken() {
+    console.log('Server URL: ', process.env.SERVER_URL)
+
     const res = await fetch(`${process.env.SERVER_URL}/api/spotify-token`, {
         next: { revalidate: 3600 },
         cache: 'no-store'
     });
-
     if (!res.ok) {
         throw new Error("Erreur lors de la récupération du token depuis l'API interne");
+
     }
 
     const data = await res.json();
-
-    console.log('Server URL: ', process.env.SERVER_URL)
     console.log("Réponse de /api/spotify-token :", data);
     return data.access_token;
 }
